19f62b1eaabceb7b738f8915f8d542c06015d401
jieun cha <flowalnut@gmail.com>
2025-04-06 02:26:25 +0900

    재수정

--- src/component/WeatherIcon.js
+++ src/component/WeatherIcon.js
@@ -1,14 +1,4 @@
 import React from 'react'
-import ashIcon from "../assets/ash.png";
-import clearIcon from "../assets/ash.png";
-import cloudIcon from "../assets/ash.png";
-import drizzleIcon from "../assets/ash.png";
-import dustIcon from "../assets/ash.png";
-import mistIcon from "../assets/ash.png";
-import rainIcon from "../assets/ash.png";
-import snowIcon from "../assets/ash.png";
-import thunderstormIcon from "../assets/ash.png";
-import tornadoIcon from "../assets/ash.png";
 
 const WeatherIcon = () => {
   return (
